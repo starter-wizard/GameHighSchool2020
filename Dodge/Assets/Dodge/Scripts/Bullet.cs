fbaa39e05266f163b4aedad4de651a5974ea9820
KimHungKyu <death_jhs@naver.com>
2020-07-01 21:18:17 +0900

    총알 피하기 게임 2일차 푸쉬

---
+++ Dodge/Assets/Dodge/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float m_Speed = 5f;
+
+    public float m_DestoryCooltime = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = /*gameObject.*/GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(transform.forward * m_Speed);
+
+        m_DestoryCooltime -= Time.deltaTime;
+
+        if (m_DestoryCooltime <= 0)
+            gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody != null && other.attachedRigidbody.tag == "Player")
+        {
+            var player = other.attachedRigidbody.GetComponent<PlayerController>();
+            player.Die();
+        }
+    }
+}
