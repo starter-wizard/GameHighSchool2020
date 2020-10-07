c465c5d08410991667e18040876deb6c583e0c4d
KimHungKyu <death_jhs@naver.com>
2020-10-07 18:56:42 +0900

    SunnyLandWood 1일차
    - CompeteCollider2D
    - PlatformEffector2D

--- AfterSchoolTest/Assets/AfterSchoolTest2/Scripts/GameManager.cs
+++ AfterSchoolTest/Assets/AfterSchoolTest2/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public CubeSpawnner m_Cubespawner;  //이거 한줄
+
     public int m_Life = 3;
     public int m_Score = 0;
 
@@ -15,6 +17,8 @@ public class GameManager : MonoBehaviour
             Instance = this;
         else
             Destroy(gameObject);
+
+        m_Cubespawner.SpawnStart();//이거 한줄
     }
 
     public void AddScore()
@@ -28,6 +32,7 @@ public class GameManager : MonoBehaviour
         if(m_Life <= 0)
         {
             //GameOver;
+            m_Cubespawner.gameObject.SetActive(false);//이거 한줄
         }
 
     }
